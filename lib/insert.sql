88ee936db89c696d299c0bcc183716dc3c200263
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-02 02:45:37 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name) VALUE ("Mr. Chocolate");
+INSERT INTO bears (name) VALUE ("Rowdy");
+INSERT INTO bears (name) VALUE ("Tabitha");
+INSERT INTO bears (name) VALUE ("Sergeant Brown");
+INSERT INTO bears (name) VALUE ("Melissa");
+INSERT INTO bears (name) VALUE ("Grinch");
+INSERT INTO bears (name) VALUE ("Wendy");
+INSERT INTO bears (name) VALUE (NULL);
